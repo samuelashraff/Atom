422c190837e3c9303054e3bd7699ca60101e39eb
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2019-10-22 00:32:14 +0300

    saatiin ohjelma soittaa mp3 tiedostoa

--- Test1/scripts.js
+++ Test1/scripts.js
@@ -1,3 +1,6 @@
+var audio = new Audio('audio/iha ok, mut ootko kattonu simpsonit sarjasta jakson himo läski homer (320 kbps) (ytmp3s.me).mp3');
+
+
 function Homersounds(){
-    
+    audio.play();
 }
